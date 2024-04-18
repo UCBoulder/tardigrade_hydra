77193714edd1376654500743a1c2d3e1a45e9f67
Nathan Miller <nami2227@colorado.edu>
2024-04-18 11:26:35 -0600

    TST: Added test for the max row preconditioner

--- src/cpp/tests/test_tardigrade_hydra.cpp
+++ src/cpp/tests/test_tardigrade_hydra.cpp
@@ -3813,3 +3813,80 @@ BOOST_AUTO_TEST_CASE( test_formPreconditioner ){
     BOOST_CHECK_THROW( bad_hydra.getFlatPreconditioner( ), std::nested_exception );
 
 }
+
+BOOST_AUTO_TEST_CASE( test_formMaxRowPreconditioner ){
+    /*!
+     * Boost test of the get-configuration command
+     */
+
+    floatType time = 1.1;
+
+    floatType deltaTime = 2.2;
+
+    floatType temperature = 5.3;
+
+    floatType previousTemperature = 23.4;
+
+    floatVector deformationGradient = { 0.39293837, -0.42772133, -0.54629709,
+                                        0.10262954,  0.43893794, -0.15378708,
+                                        0.9615284 ,  0.36965948, -0.0381362 };
+
+    floatVector previousDeformationGradient = { -0.21576496, -0.31364397,  0.45809941,
+                                                -0.12285551, -0.88064421, -0.20391149,
+                                                 0.47599081, -0.63501654, -0.64909649 };
+
+    floatVector previousStateVariables = { 0.53155137, 0.53182759, 0.63440096, 0.84943179, 0.72445532,
+                                           0.61102351, 0.72244338, 0.32295891, 0.36178866, 0.22826323,
+                                           0.29371405, 0.63097612, 0.09210494, 0.43370117, 0.43086276,
+                                           0.4936851 , 0.42583029, 0.31226122, 0.42635131, 0.89338916,
+                                           0.94416002, 0.50183668, 0.62395295, 0.1156184 , 0.31728548,
+                                           0.41482621, 0.86630916, 0.25045537, 0.48303426, 0.98555979,
+                                           0.51948512, 0.61289453, 0.12062867, 0.8263408 , 0.60306013,
+                                           0.54506801, 0.34276383, 0.30412079 }; 
+
+    floatVector parameters = { 1, 2, 3, 4, 5 };
+
+    unsigned int numConfigurations = 4;
+
+    unsigned int numNonLinearSolveStateVariables = 5;
+
+    unsigned int dimension = 3;
+
+    class hydraBaseMock : public tardigradeHydra::hydraBase{
+
+        public:
+
+            floatVector jacobian = {  1.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+                                      0.        ,  1.        ,  0.        ,  0.        ,  0.        ,
+                                      0.        , 48.07641984,  1.        ,  0.        , -7.68935399,
+                                      0.        ,  0.        , 18.48297386,  1.        ,  0.        ,
+                                      0.        ,  0.        ,  0.        ,  0.        ,  1.        };
+
+            using tardigradeHydra::hydraBase::hydraBase;
+
+            void setPreconditionerType( const unsigned int val ){
+
+                tardigradeHydra::unit_test::hydraBaseTester::set_preconditionerType( *this, val );
+
+            }
+
+            virtual void formMaxRowPreconditioner( ) override{
+
+                tardigradeHydra::unit_test::hydraBaseTester::set_unknownVector( *this, floatVector( 5, 0 ) );
+
+                tardigradeHydra::unit_test::hydraBaseTester::set_flatJacobian( *this, jacobian );
+
+                tardigradeHydra::hydraBase::formMaxRowPreconditioner( );
+
+            }
+
+    };
+
+    hydraBaseMock hydra( time, deltaTime, temperature, previousTemperature, deformationGradient, previousDeformationGradient,
+                         previousStateVariables, parameters, numConfigurations, numNonLinearSolveStateVariables, dimension );
+
+    floatVector answer = { 1.        , 1.        , 0.02080022, 0.05410385, 1.        };
+
+    BOOST_CHECK( tardigradeVectorTools::fuzzyEquals( answer, *hydra.getFlatPreconditioner( ) ) );
+
+}
