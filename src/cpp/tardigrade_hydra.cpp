03d25a0ccd157d178822e61d8f131ea39a606047
Nathan Miller <nami2227@colorado.edu>
2025-10-07 13:58:35 -0600

    MAINT: Setting the accessible relaxationIteration variable

--- src/cpp/tardigrade_hydra.cpp
+++ src/cpp/tardigrade_hydra.cpp
@@ -2098,6 +2098,7 @@ namespace tardigradeHydra{
          */
 
         unsigned int relaxedIteration = 0;
+        setRelaxedIteration( relaxedIteration );
 
         // Initialize the residuals
         setCurrentResidualIndexMeaningful( true );
@@ -2150,6 +2151,7 @@ namespace tardigradeHydra{
             }
 
             relaxedIteration++;
+            setRelaxedIteration( relaxedIteration );
 
             // Initialize the residuals
             setCurrentResidualIndexMeaningful( true );
