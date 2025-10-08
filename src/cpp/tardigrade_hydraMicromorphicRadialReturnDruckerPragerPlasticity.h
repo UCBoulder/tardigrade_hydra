a55e1bf86c431779e8bc9aeb1d2f1357c587956b
Nathan Miller <nami2227@colorado.edu>
2025-10-08 08:30:04 -0600

    MAINT: Minor change to formatting

--- src/cpp/tardigrade_hydraMicromorphicRadialReturnDruckerPragerPlasticity.h
+++ src/cpp/tardigrade_hydraMicromorphicRadialReturnDruckerPragerPlasticity.h
@@ -163,7 +163,7 @@ namespace tardigradeHydra{
                      * Get the scale factor for the global tolerance if the constraints have changed
                      */
 
-                    return 10;
+                    return 10.0;
 
                 }
 
