20387b84d12bb0a957a3d10b1a2f9931d14d9c53
Nathan Miller <nami2227@colorado.edu>
2025-11-20 13:52:58 -0700

    WIP: Working towards finishing the subconfiguration gradient Jacobians

--- src/cpp/tardigrade_DeformationBase.h
+++ src/cpp/tardigrade_DeformationBase.h
@@ -121,6 +121,46 @@ namespace tardigradeHydra{
                 output_iterator output_begin, output_iterator output_end
             );
 
+            template<
+                unsigned int size,
+                unsigned int dim,
+                class configuration_iterator,
+                class configuration_gradient_iterator,
+                class output_iterator
+            >
+            void getLeadingSubConfigurationGradientConfigurationGradientJacobian(
+                const configuration_iterator &configurations_begin, const configuration_iterator &configurations_end,
+                const configuration_gradient_iterator &configuration_gradients_begin, const configuration_gradient_iterator &configuration_gradients_end,
+                output_iterator output_begin, output_iterator output_end
+            );
+
+            template<
+                unsigned int size,
+                unsigned int dim,
+                class configuration_iterator,
+                class configuration_gradient_iterator,
+                class output_iterator
+            >
+            void getTrailingSubConfigurationGradientConfigurationGradientJacobian(
+                const configuration_iterator &configurations_begin, const configuration_iterator &configurations_end,
+                const configuration_gradient_iterator &configuration_gradients_begin, const configuration_gradient_iterator &configuration_gradients_end,
+                output_iterator output_begin, output_iterator output_end
+            );
+
+            template<
+                unsigned int size,
+                unsigned int dim,
+                class configuration_iterator,
+                class configuration_gradient_iterator,
+                class output_iterator
+            >
+            void getSubConfigurationGradientConfigurationGradientJacobian(
+                const configuration_iterator &configurations_begin, const configuration_iterator &configurations_end,
+                const configuration_gradient_iterator &configuration_gradients_begin, const configuration_gradient_iterator &configuration_gradients_end,
+                const unsigned int &configuration_index,
+                output_iterator output_begin, output_iterator output_end
+            );
+
         protected:
 
             template<
