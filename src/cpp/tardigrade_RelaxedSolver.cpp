478372717e71b133c6b510ec7e0fb901e9db87a0
Nathan Miller <nami2227@colorado.edu>
2026-01-08 08:36:19 -0700

    FEAT: Initial commit of the RelaxedSolver class

---
+++ src/cpp/tardigrade_RelaxedSolver.cpp
@@ -0,0 +1,15 @@
+/**
+  ******************************************************************************
+  * \file tardigrade_RelaxedSolver.cpp
+  ******************************************************************************
+  * A C++ library for the nonlinear solvers which attempt to relax the problem
+  * during its solution
+  ******************************************************************************
+  */
+
+#include"tardigrade_RelaxedSolver.h"
+#include"tardigrade_hydra.h"
+
+namespace tardigradeHydra{
+
+}
---
+++ src/cpp/tardigrade_RelaxedSolver.h
@@ -0,0 +1,20 @@
+/**
+  ******************************************************************************
+  * \file tardigrade_RelaxedSolver.h
+  ******************************************************************************
+  * A C++ library for the nonlinear solvers which attempt to relax the problem
+  * during its solution
+  ******************************************************************************
+  */
+
+#ifndef TARDIGRADE_RELAXEDSOLVER_H
+#define TARDIGRADE_RELAXEDSOLVER_H
+
+#include"tardigrade_CoreDefinitions.h"
+#include"tardigrade_SolverBase.h"
+
+namespace tardigradeHydra{
+
+}
+
+#endif
