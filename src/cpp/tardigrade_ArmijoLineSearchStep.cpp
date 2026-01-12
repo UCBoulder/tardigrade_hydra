03adc0b9623e174c9488876d9faff007fd2cc26a
Nathan Miller <nami2227@colorado.edu>
2026-01-12 15:24:34 -0700

    WIP: Added the placeholder for the Armijo line search step functionality

---
+++ src/cpp/tardigrade_ArmijoLineSearchStep.cpp
@@ -0,0 +1,13 @@
+/**
+  ******************************************************************************
+  * \file tardigrade_ArmijoLineSearchStep.cpp
+  ******************************************************************************
+  * A C++ library for the Armijo line search step
+  ******************************************************************************
+  */
+
+#include"tardigrade_ArmijoLineSearchStep.h"
+
+namespace tardigradeHydra{
+
+}
---
+++ src/cpp/tardigrade_ArmijoLineSearchStep.h
@@ -0,0 +1,34 @@
+/**
+  ******************************************************************************
+  * \file tardigrade_ArmijoLineSearchStep.h
+  ******************************************************************************
+  * A C++ library for the Armijo line search step
+  ******************************************************************************
+  */
+
+#ifndef TARDIGRADE_ARMIJOLINESEARCHSTEP_H
+#define TARDIGRADE_ARMIJOLINESEARCHSTEP_H
+
+#include"tardigrade_CoreDefinitions.h"
+#include"tardigrade_SolverBase.h"
+
+namespace tardigradeHydra{
+
+    /*!
+     * The Armijo line search step class
+     */
+    class ArmijoLineSearchStep : public SolverStepBase {
+
+        public:
+
+            using tardigradeHydra::SolverStepBase::SolverStepBase;
+
+        protected:
+
+        private:
+
+    };
+
+}
+
+#endif
