1293b8df312811d7bcf1d3e9388306a0d5070e2d
Nathan Miller <nami2227@colorado.edu>
2026-01-12 13:03:29 -0700

    MAINT: Updated the Levenberg Marquardt Solver to enable convergence

---
+++ src/cpp/tardigrade_LevenbergMarquardtSolver.cpp
@@ -0,0 +1,13 @@
+/**
+  ******************************************************************************
+  * \file tardigrade_LevenbergMarquardtSolver.cpp
+  ******************************************************************************
+  * A C++ library for the Levenberg Marquardt solver
+  ******************************************************************************
+  */
+
+#include"tardigrade_LevenbergMarquardtSolver.h"
+
+namespace tardigradeHydra{
+
+}
