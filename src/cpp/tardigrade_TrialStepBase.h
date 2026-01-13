e3011d212ed4f6c07e97a4e2171e20e9eb6380d3
Nathan Miller <nami2227@colorado.edu>
2026-01-13 10:47:48 -0700

    MAINT: Added base classes for determining the initial trial step and applying damping

---
+++ src/cpp/tardigrade_TrialStepBase.h
@@ -0,0 +1,57 @@
+/**
+ ******************************************************************************
+ * \file tardigrade_TrialStepBase.h
+ ******************************************************************************
+ * The base class for trial step proposal operations
+ ******************************************************************************
+ */
+
+#ifndef TARDIGRADE_TRIALSTEPBASE
+#define TARDIGRADE_TRIALSTEPBASE
+
+#include"tardigrade_CoreDefinitions.h"
+#include"tardigrade_SetDataStorage.h"
+
+namespace tardigradeHydra{
+
+    /*!
+     * The base class for step damping operations to improve
+     * stability
+     */
+    class TrialStepBase : public CachingDataBase{
+
+        public:
+
+            /*!
+             * The constructor for TrialStepBase
+             */
+            TrialStepBase( ) : step(NULL){
+
+            }
+
+            /*!
+             * The constructor for TrialStepBase
+             *
+             * \param *_step: The containing step object
+             */
+
+            TrialStepBase( SolverStepBase *_step ) : step( _step ){
+
+            }
+
+            SolverStepBase *step; //!< The containing step class
+
+            /*!
+             * Compute the trial step
+             */
+            void computeTrial( );
+
+        protected:
+
+        private:
+
+    };
+
+}
+
+#endif
