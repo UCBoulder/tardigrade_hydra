e20d5d4e16239317916c98f81f4664964f21b452
Nathan Miller <nami2227@colorado.edu>
2026-01-21 13:32:33 -0700

    MAINT: Initial commit

---
+++ src/cpp/tardigrade_NonlinearSolverBase.cpp
@@ -0,0 +1,13 @@
+/**
+ ******************************************************************************
+ * \file tardigrade_NonlinearSolverBase.cpp
+ ******************************************************************************
+ * The base class for nonlinear solver classes
+ ******************************************************************************
+ */
+
+#include"tardigrade_NonlinearSolverBase.h"
+
+namespace tardigradeHydra{
+
+}
---
+++ src/cpp/tardigrade_NonlinearSolverBase.h
@@ -0,0 +1,34 @@
+/**
+ ******************************************************************************
+ * \file tardigrade_NonlinearSolverBase.h
+ ******************************************************************************
+ * The base class for nonlinear solver objects
+ ******************************************************************************
+ */
+
+#ifndef TARDIGRADE_NONLINEARSOLVERBASE
+#define TARDIGRADE_NONLINEARSOLVERBASE
+
+#include"tardigrade_SolverBase.h"
+
+namespace tardigradeHydra{
+
+    namespace unit_test{
+
+        class NonlinearSolveBaseTester;
+
+    }
+
+    /*!
+     * The base class for step damping operations to improve
+     * stability
+     */
+    class NonlinearSolverBase : public SolverBase{
+
+        public:
+
+    };
+
+}
+
+#endif
