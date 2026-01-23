1a4499c266f80dfc31a75ee2f4e7fc483ad7be3d
Nathan Miller <nami2227@colorado.edu>
2026-01-23 08:13:11 -0700

    MAINT: Initial commit of NonlinearStepBase

---
+++ src/cpp/tardigrade_NonlinearStepBase.h
@@ -0,0 +1,33 @@
+/**
+  ******************************************************************************
+  * \file tardigrade_NonlinearStepBase.h
+  ******************************************************************************
+  * The header file for the base class to determine the nonlinear step
+  ******************************************************************************
+  */
+
+#ifndef TARDIGRADE_NONLINEARSTEPBASE_H
+#define TARDIGRADE_NONLINEARSTEPBASE_H
+
+#include"tardigrade_TrialStepBase.h"
+
+namespace tardigradeHydra{
+
+    /*!
+     * The base nonlinear step class
+     */
+    class NonlinearStepBase : public TrialStepBase{
+
+        public:
+
+            using tardigradeHydra::TrialStepBase::TrialStepBase;
+
+        protected:
+
+        private:
+
+    };
+
+}
+
+#endif
