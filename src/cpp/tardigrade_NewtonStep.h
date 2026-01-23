0befac32cc0e314c2bd5b2c97fb8cc23e402da0c
Nathan Miller <nami2227@colorado.edu>
2026-01-23 13:57:52 -0700

    MAINT: Initial commit

---
+++ src/cpp/tardigrade_NewtonStep.h
@@ -0,0 +1,18 @@
+/**
+ ******************************************************************************
+ * \file tardigrade_NewtonStep.h
+ ******************************************************************************
+ * A class which defines a Newton-Raphson step
+ ******************************************************************************
+ */
+
+#ifndef TARDIGRADE_HYDRA_NEWTONSTEP
+#define TARDIGRADE_HYDRA_NEWTONSTEP
+
+#include "tardigrade_NonlinearStepBase.h"
+
+namespace tardigradeHydra {
+
+}
+
+#endif
