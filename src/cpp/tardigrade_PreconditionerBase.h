609c126ea449e48c53413a5a4eed7fc0ac5cb6ec
Nathan Miller <nami2227@colorado.edu>
2026-01-26 11:01:20 -0700

    MAINT: Removed preconditioner diagonal flag

--- src/cpp/tardigrade_PreconditionerBase.h
+++ src/cpp/tardigrade_PreconditionerBase.h
@@ -38,9 +38,6 @@ namespace tardigradeHydra {
         //! Get the preconditioner type
         constexpr unsigned int getPreconditionerType() { return _preconditioner_type; }
 
-        //! Get whether the preconditioner is diagonal or not
-        constexpr bool getPreconditionerIsDiagonal() { return _preconditioner_is_diagonal; }
-
         //! Get a pointer to the row-major form of the preconditioner
         const floatVector *getFlatPreconditioner();
 
@@ -58,8 +55,6 @@ namespace tardigradeHydra {
         bool _use_preconditioner;  //!< Flag for whether to pre-condition the Jacobian or not
 
         unsigned int _preconditioner_type = 0;  //!< The type of preconditioner to use
-
-        bool _preconditioner_is_diagonal = true;  //!< Flag for if the pre-conditioner only stores the diagonal elements
 
         virtual void formMaxRowPreconditioner();  //!< Temporary function. We won't use it in the future.
         // END TEMP
