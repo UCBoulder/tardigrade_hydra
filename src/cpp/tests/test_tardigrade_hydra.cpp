bccc506bda2f16107333e487cda231bedc2ccfe3
Nathan Miller <nami2227@colorado.edu>
2026-01-29 17:02:55 -0700

    MAINT: Extracted SubcyclerSolver tests to its source file

--- src/cpp/tests/test_tardigrade_hydra.cpp
+++ src/cpp/tests/test_tardigrade_hydra.cpp
@@ -3777,6 +3777,8 @@ BOOST_AUTO_TEST_CASE(test_hydraBase_evaluate3, *boost::unit_test::tolerance(DEFA
 
         unsigned int n_failure = 3;
 
+        unsigned int num_callResidualPostSubcyclerSuccessCalls = 0;
+
         unsigned int num_callResidualPostSubcyclerFailureCalls = 0;
 
         virtual void solve() override {
@@ -3786,6 +3788,8 @@ BOOST_AUTO_TEST_CASE(test_hydraBase_evaluate3, *boost::unit_test::tolerance(DEFA
             }
             num_solverCalls++;
         }
+
+        virtual void callResidualPostSubcyclerSuccess() override { num_callResidualPostSubcyclerSuccessCalls++; }
 
         virtual void callResidualPostSubcyclerFailure() override { num_callResidualPostSubcyclerFailureCalls++; }
 
@@ -3802,8 +3806,6 @@ BOOST_AUTO_TEST_CASE(test_hydraBase_evaluate3, *boost::unit_test::tolerance(DEFA
         unsigned int num_callResidualPreSubcyclerCalls = 0;
 
         unsigned int num_resetProblemCalls = 0;
-
-        unsigned int num_callResidualPostSubcyclerSuccessCalls = 0;
 
         unsigned int num_setScaleFactorCalls = 0;
 
@@ -3827,8 +3829,6 @@ BOOST_AUTO_TEST_CASE(test_hydraBase_evaluate3, *boost::unit_test::tolerance(DEFA
         virtual void callResidualPreSubcycler() override { num_callResidualPreSubcyclerCalls++; }
 
         virtual void resetProblem() override { num_resetProblemCalls++; }
-
-        virtual void callResidualPostSubcyclerSuccess() override { num_callResidualPostSubcyclerSuccessCalls++; }
 
         virtual void setScaleFactor(const floatType &value) override { num_setScaleFactorCalls++; }
 
@@ -3888,7 +3888,7 @@ BOOST_AUTO_TEST_CASE(test_hydraBase_evaluate3, *boost::unit_test::tolerance(DEFA
 
     BOOST_TEST(hydra.num_setScaleFactorCalls == 8);
 
-    BOOST_TEST(hydra.num_callResidualPostSubcyclerSuccessCalls == 6);
+    BOOST_TEST(solver.num_callResidualPostSubcyclerSuccessCalls == 6);
 
     BOOST_TEST(solver.num_callResidualPostSubcyclerFailureCalls == 2);
 }
@@ -4119,208 +4119,6 @@ BOOST_AUTO_TEST_CASE(test_hydraBase_callResidualPreSubcycler, *boost::unit_test:
     BOOST_TEST(hydra.r2.numPreSubcyclerCalls == 1);
 
     BOOST_TEST(hydra.r3.numPreSubcyclerCalls == 1);
-}
-
-BOOST_AUTO_TEST_CASE(test_hydraBase_callResidualSubcyclerSuccess,
-                     *boost::unit_test::tolerance(DEFAULT_TEST_TOLERANCE)) {
-    class residualMock : public tardigradeHydra::ResidualBase<tardigradeHydra::hydraBase> {
-       public:
-        using tardigradeHydra::ResidualBase<tardigradeHydra::hydraBase>::ResidualBase;
-
-        unsigned int numPostSubcyclerSuccessCalls = 0;
-
-        virtual void postSubcyclerSuccess() override { numPostSubcyclerSuccessCalls++; }
-
-       protected:
-    };
-
-    class hydraBaseMock : public tardigradeHydra::hydraBase {
-       public:
-        residualMock r1;
-
-        residualMock r2;
-
-        residualMock r3;
-
-        unsigned int s1 = 36;
-
-        unsigned int s2 = 2;
-
-        unsigned int s3 = 3;
-
-        using tardigradeHydra::hydraBase::hydraBase;
-
-        using tardigradeHydra::hydraBase::setResidualClasses;
-
-        virtual void setResidualClasses() {
-            r1 = residualMock(this, s1);
-
-            r2 = residualMock(this, s2);
-
-            r3 = residualMock(this, s3);
-
-            std::vector<tardigradeHydra::ResidualBase<tardigradeHydra::hydraBase> *> residuals(3);
-
-            residuals[0] = &r1;
-
-            residuals[1] = &r2;
-
-            residuals[2] = &r3;
-
-            setResidualClasses(residuals);
-        }
-
-        virtual void public_callResidualPostSubcyclerSuccess() { callResidualPostSubcyclerSuccess(); }
-    };
-
-    floatType time = 1.1;
-
-    floatType deltaTime = 2.2;
-
-    floatType temperature = 5.3;
-
-    floatType previousTemperature = 23.4;
-
-    floatVector deformationGradient = {0.39293837,  -0.42772133, -0.54629709, 0.10262954, 0.43893794,
-                                       -0.15378708, 0.9615284,   0.36965948,  -0.0381362};
-
-    floatVector previousDeformationGradient = {-0.21576496, -0.31364397, 0.45809941,  -0.12285551, -0.88064421,
-                                               -0.20391149, 0.47599081,  -0.63501654, -0.64909649};
-
-    floatVector previousStateVariables = {
-        0.53155137, 0.53182759, 0.63440096, 0.84943179, 0.72445532, 0.61102351, 0.72244338, 0.32295891,
-        0.36178866, 0.22826323, 0.29371405, 0.63097612, 0.09210494, 0.43370117, 0.43086276, 0.4936851,
-        0.42583029, 0.31226122, 0.42635131, 0.89338916, 0.94416002, 0.50183668, 0.62395295, 0.1156184,
-        0.31728548, 0.41482621, 0.86630916, 0.25045537, 0.48303426, 0.98555979, 0.51948512, 0.61289453,
-        0.12062867, 0.8263408,  0.60306013, 0.54506801, 0.34276383, 0.30412079, 0.0,        0.1};
-
-    floatVector parameters = {1, 2, 3, 4, 5};
-
-    unsigned int numConfigurations = 4;
-
-    unsigned int numNonLinearSolveStateVariables = 5;
-
-    unsigned int dimension = 3;
-
-    hydraBaseMock hydra(time, deltaTime, temperature, previousTemperature, deformationGradient,
-                        previousDeformationGradient, {}, {}, previousStateVariables, parameters, numConfigurations,
-                        numNonLinearSolveStateVariables, dimension);
-
-    hydra.public_callResidualPostSubcyclerSuccess();
-
-    BOOST_TEST(hydra.r1.numPostSubcyclerSuccessCalls == 1);
-
-    BOOST_TEST(hydra.r2.numPostSubcyclerSuccessCalls == 1);
-
-    BOOST_TEST(hydra.r3.numPostSubcyclerSuccessCalls == 1);
-}
-
-BOOST_AUTO_TEST_CASE(test_hydraBase_callResidualSubcyclerFailure,
-                     *boost::unit_test::tolerance(DEFAULT_TEST_TOLERANCE)) {
-    class residualMock : public tardigradeHydra::ResidualBase<tardigradeHydra::hydraBase> {
-       public:
-        using tardigradeHydra::ResidualBase<tardigradeHydra::hydraBase>::ResidualBase;
-
-        unsigned int numPostSubcyclerFailureCalls = 0;
-
-        virtual void postSubcyclerFailure() override { numPostSubcyclerFailureCalls++; }
-
-       protected:
-    };
-
-    class SubcyclerSolverMock : public tardigradeHydra::SubcyclerSolver {
-
-        public:
-            using tardigradeHydra::SubcyclerSolver::SubcyclerSolver;
-
-            virtual void public_callResidualPostSubcyclerFailure() { callResidualPostSubcyclerFailure(); }
-    };
-
-    class hydraBaseMock : public tardigradeHydra::hydraBase {
-       public:
-        residualMock r1;
-
-        residualMock r2;
-
-        residualMock r3;
-
-        unsigned int s1 = 36;
-
-        unsigned int s2 = 2;
-
-        unsigned int s3 = 3;
-
-        using tardigradeHydra::hydraBase::hydraBase;
-
-        using tardigradeHydra::hydraBase::setResidualClasses;
-
-        virtual void setResidualClasses() {
-            r1 = residualMock(this, s1);
-
-            r2 = residualMock(this, s2);
-
-            r3 = residualMock(this, s3);
-
-            std::vector<tardigradeHydra::ResidualBase<tardigradeHydra::hydraBase> *> residuals(3);
-
-            residuals[0] = &r1;
-
-            residuals[1] = &r2;
-
-            residuals[2] = &r3;
-
-            setResidualClasses(residuals);
-        }
-
-        void setSolver(tardigradeHydra::SubcyclerSolver *_solver){solver=_solver;}
-        auto getSolver(){return solver;}
-
-    };
-
-    floatType time = 1.1;
-
-    floatType deltaTime = 2.2;
-
-    floatType temperature = 5.3;
-
-    floatType previousTemperature = 23.4;
-
-    floatVector deformationGradient = {0.39293837,  -0.42772133, -0.54629709, 0.10262954, 0.43893794,
-                                       -0.15378708, 0.9615284,   0.36965948,  -0.0381362};
-
-    floatVector previousDeformationGradient = {-0.21576496, -0.31364397, 0.45809941,  -0.12285551, -0.88064421,
-                                               -0.20391149, 0.47599081,  -0.63501654, -0.64909649};
-
-    floatVector previousStateVariables = {
-        0.53155137, 0.53182759, 0.63440096, 0.84943179, 0.72445532, 0.61102351, 0.72244338, 0.32295891,
-        0.36178866, 0.22826323, 0.29371405, 0.63097612, 0.09210494, 0.43370117, 0.43086276, 0.4936851,
-        0.42583029, 0.31226122, 0.42635131, 0.89338916, 0.94416002, 0.50183668, 0.62395295, 0.1156184,
-        0.31728548, 0.41482621, 0.86630916, 0.25045537, 0.48303426, 0.98555979, 0.51948512, 0.61289453,
-        0.12062867, 0.8263408,  0.60306013, 0.54506801, 0.34276383, 0.30412079, 0.0,        0.1};
-
-    floatVector parameters = {1, 2, 3, 4, 5};
-
-    unsigned int numConfigurations = 4;
-
-    unsigned int numNonLinearSolveStateVariables = 5;
-
-    unsigned int dimension = 3;
-
-    hydraBaseMock hydra(time, deltaTime, temperature, previousTemperature, deformationGradient,
-                        previousDeformationGradient, {}, {}, previousStateVariables, parameters, numConfigurations,
-                        numNonLinearSolveStateVariables, dimension);
-
-    SubcyclerSolverMock solver;
-    solver.hydra = &hydra;
-    hydra.setSolver(&solver);
-
-    solver.public_callResidualPostSubcyclerFailure();
-
-    BOOST_TEST(hydra.r1.numPostSubcyclerFailureCalls == 1);
-
-    BOOST_TEST(hydra.r2.numPostSubcyclerFailureCalls == 1);
-
-    BOOST_TEST(hydra.r3.numPostSubcyclerFailureCalls == 1);
 }
 
 BOOST_AUTO_TEST_CASE(test_hydraBase_getCurrentResidualOffset, *boost::unit_test::tolerance(DEFAULT_TEST_TOLERANCE)) {
