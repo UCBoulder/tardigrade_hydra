59a20f17729b9d5b88d6c00c1e6dff14a0afbe37
Nathan Miller <nami2227@colorado.edu>
2026-02-09 16:08:06 -0700

    MAINT: Added missing caching data storage operators

--- src/cpp/tardigrade_DeformationBase.h
+++ src/cpp/tardigrade_DeformationBase.h
@@ -24,6 +24,13 @@ namespace tardigradeHydra {
 
         public:
 
+            /*!
+             * Constructor of DeformationBase
+             *
+             * \param *_hydra: The containing hydra object
+             */
+            DeformationBase(hydraBase *_hydra=nullptr) : hydra(_hydra) { }
+
             template<unsigned int leading_rows, unsigned int size, unsigned int dim>
             floatVector getSubConfiguration(const floatVector &configurations, const unsigned int &lowerIndex,
                                             const unsigned int &upperIndex);
@@ -31,14 +38,33 @@ namespace tardigradeHydra {
             template<unsigned int leading_rows, unsigned int size, unsigned int dim>
             floatVector getSubConfigurationJacobian(const floatVector  &configurations,
                                                     const unsigned int &lowerIndex, const unsigned int &upperIndex);
+
+            // CACHED DATA STORAGE OPERATIONS
+            virtual void addIterationData(dataBase *data) override;
+
+            virtual void addNLStepData(dataBase *data) override;
+            // END CACHED DATA STORAGE OPERATIONS
+
+            //! Pointer to the containing hydraBase object
+            hydraBase *hydra;
+
         protected:
 
         private:
+            friend class unit_test::DeformationBaseTester;  //!< Friend class which allows modification of private variables. ONLY
+                                                            //!< TO BE USED FOR TESTING!
+
+            //! A pass through function that does nothing
+            void passThrough() {}
+
+            TARDIGRADE_HYDRA_DECLARE_ITERATION_STORAGE(private, configurations, floatVector, passThrough)
+
+            TARDIGRADE_HYDRA_DECLARE_PREVIOUS_STORAGE(private, previousConfigurations, floatVector, passThrough)
 
     };
 
 }
 
-#include "tardigrade_DeformationBase.cpp"
+#include "tardigrade_DeformationBase.tpp"
 
 #endif
